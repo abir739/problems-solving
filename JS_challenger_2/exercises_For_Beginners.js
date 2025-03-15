12fcbf20246f3056e6789221a14b01fc2d411bcf
abir cherif <abir.cherif98@gmail.com>
2025-03-15 13:29:39 +0100

    JavaScript Practice Exercises For Beginners

--- JS_challenger_2/exercises_For_Beginners.js
+++ JS_challenger_2/exercises_For_Beginners.js
@@ -77,3 +77,52 @@ If the given string begins with "New!" already then return the original string.
 const addingNew = (str) =>  /^New!/.test(str) ? str : 'New! ' + str;
 
 console.log(addingNew('dayNew!'));
+
+// Exercice 6: 
+/* write a JS program to create new string from a given string taking 
+the first 3 characters and the last 3 characters of string and adding
+them together. the string length must be 3 or more, if not, the original string is returned.
+*/
+
+const createNewString = (str) => str.length < 3 ? str : str.slice(0,3) + str.slice(-3);
+ console.log(createNewString('abc123abc123'));
+
+// Exercice 7: 
+/* write a JS program to extract the first half of a string of even length
+*/
+ const halfString = (str) => str.slice(0, str.length / 2);
+
+ console.log(halfString('abcdef'));
+
+ // Exercice 8: 
+/* write a JS program to concatenate two strings except their first character
+*/
+
+
+const concatenateStrings = (str1, str2) => str1.slice(1) + str2.slice(1);
+
+console.log(concatenateStrings('abc', 'def'));
+
+// Exercice 9: 
+/* Given two value,
+write a JS program to find out which one is nearest to 100
+*/
+
+const nearestTo100 = (val1, val2) => Math.abs(val1 - 100) < Math.abs(val2 - 100) ? val1 : val2;
+
+console.log(nearestTo100(110, 60));
+
+// Exercice 10: 
+/* write a JS program to check a given string contains 2 to 4 occurrences
+of a specified character.*/
+
+function contains2To4(str, char) {
+    const countChars = (str, char) => str.split('').filter(c => c === char).length;
+
+    return countChars(str, char) >=2 && countChars(str, char) <= 4;
+}
+
+console.log(contains2To4('ooh!', 'o'));
+console.log(contains2To4('ooooh!', 'o'));
+console.log(contains2To4('oooooh!', 'o'));
+console.log(contains2To4('oh!', 'o'));
